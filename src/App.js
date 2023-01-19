29d88e1fa64978135f9d6829951cb0c33b71058b
jooedvard7 <jooedvard7@gmail.com>
2023-01-19 18:41:07 +0100

    gradiant

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Quiz from "./components/Questions/Quiz";
 
 function App() {
   return (
-    <div className="App p-5 sm:p-10 bg-cyan-50 flex min-h-screen h-full ">
+    <div className="App p-5 sm:p-10 flex min-h-screen h-full ">
       <Quiz></Quiz>
     </div>
   );
