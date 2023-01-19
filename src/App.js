7bf2fa2b7d718ef596bcbf30a3e399668edd300c
jooedvard7 <jooedvard7@gmail.com>
2023-01-19 21:33:09 +0100

    small bubbles

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ function App() {
   return (
     <div className="App p-5  sm:p-10 flex min-h-screen h-full ">
       <Quiz></Quiz>
-      <div>
-     <div class="wave"></div>
-     <div class="wave"></div>
-     <div class="wave"></div>
-  </div>
+      <div id="background-wrap">
+        <div class="bubble x1"></div>
+        <div class="bubble x2"></div>
+        <div class="bubble x3"></div>
+        <div class="bubble x4"></div>
+        <div class="bubble x5"></div>
+        <div class="bubble x6"></div>
+        <div class="bubble x7"></div>
+        <div class="bubble x8"></div>
+        <div class="bubble x9"></div>
+        <div class="bubble x10"></div>
+      </div>
     </div>
   );
 }
