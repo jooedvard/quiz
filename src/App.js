fe6d379c0eaf4b99fc25da01ba65e43126472be7
jooedvard7 <jooedvard7@gmail.com>
2023-01-19 18:41:20 +0100

    adding gradient

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App p-5 sm:p-10 flex min-h-screen h-full ">
       <Quiz></Quiz>
+      <div>
+     <div class="wave"></div>
+     <div class="wave"></div>
+     <div class="wave"></div>
+  </div>
     </div>
   );
 }
